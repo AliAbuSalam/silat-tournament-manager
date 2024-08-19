f0d2e4a344eaf5eb65479038ca5ecaa8736ec520
Ali Abu Salam <aliadadehmav@gmail.com>
2024-08-19 21:28:28 +0700

    add custom main color on tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        "main-color": "#4169e1",
+        "main-color-darker": "#3a5eca"
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
