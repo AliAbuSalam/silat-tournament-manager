d89dcde9e47442fc9b356715e54f0dda049d7014
Ali Abu Salam <aliadadehmav@gmail.com>
2024-08-27 21:23:34 +0700

    add initial component for showing list of events

--- src/components/EventBox/index.tsx
+++ src/components/EventBox/index.tsx
@@ -1,17 +1,29 @@
+'use client'
+import { useState } from 'react';
+
 const EventBox = ({ title }: {
   title: string
 }) => {
+  const [open, setOpen] = useState(true);
+  const handleCollapse = () => {
+    setOpen(!open)
+  }
   return(
     <div className="outline outline-main-color w-11/12 my-4  mx-auto rounded-lg">
-      <div className="bg-main-color text-slate-200 hover:text-white hover:bg-main-color-darker
-      py-2 px-2 cursor-pointer flex flex-row justify-between">
-        <h1 className="text-2xl text-inherit w-fit">{title}</h1>
-        <span className="material-symbols-outlined text-[36px]">keyboard_arrow_down</span>
+      <div 
+        onClick={handleCollapse}
+        className="bg-main-color text-slate-200 hover:text-white hover:bg-main-color-darker
+        py-2 px-2 cursor-pointer flex flex-row justify-between">
+          <h1 className="text-2xl text-inherit w-fit">{title}</h1>
+          <span className="material-symbols-outlined text-[36px]">{open ? 'keyboard_arrow_up': 'keyboard_arrow_down'}</span>
       </div>
-      test<br />
-      test<br />
-      test<br />
-      test<br />
+      <div className={open ? "": "hidden"}>
+        test<br/>
+        test<br/>
+        test<br/>
+        test<br/>
+        test<br/>
+      </div>
     </div>
   );
 };
